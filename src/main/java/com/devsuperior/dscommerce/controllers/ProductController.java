8e6d17defa9d94dfcf16dadcd1fe426c0b65cf24
Yan Hideki Kawakami <yanhkawakami@gmail.com>
2025-11-04 20:18:17 -0300

    add: adicao do service corrigido

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,11 +25,13 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+
     @GetMapping (value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
+
 
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> findAll(
@@ -37,6 +40,7 @@ public class ProductController {
         Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
+
 
     @PostMapping
     // O @Valid é a notação do Jakarta que sinaliza que há uma validação a ser feita no DTO
@@ -48,12 +52,14 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+
     @PutMapping (value = "/{id}")
     // O @Valid é a notação do Jakarta que sinaliza que há uma validação a ser feita no DTO
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO){
         ProductDTO dto = service.update(id, productDTO);
         return ResponseEntity.ok(dto);
     }
+
 
     @DeleteMapping (value = "/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,4 +1,37 @@
 package com.devsuperior.dscommerce.services;
 
-public class UserService {
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import com.devsuperior.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // Dessa forma, conseguimos controlar o retorno do banco de dados e retornar a lista conforme desejado
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0){
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.getFirst().getPassword());
+        for (UserDetailsProjection userDetailsProjection : result){
+            user.addRole(new Role(userDetailsProjection.getRoleId(), userDetailsProjection.getAuthority()));
+        }
+        return user;
+    }
 }
